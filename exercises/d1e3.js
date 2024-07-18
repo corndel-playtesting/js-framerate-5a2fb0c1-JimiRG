d52a3d4109890d6c1c9cffe77f418896e9a4c903
JimiRG <jimi.gill@outlook.com>
2024-07-18 13:48:34 +0100

    solved exercises

--- exercises/d1e3.js
+++ exercises/d1e3.js
@@ -4,6 +4,7 @@ const app = express()
 app.set('views', 'exercises/views') // <-- Don't change
 
 // TODO: Configure the app to use EJS as the view engine
+app.set('view engine', 'ejs')
 
 app.get('/d1e3', (req, res) => {
   const shopping = [
@@ -14,6 +15,7 @@ app.get('/d1e3', (req, res) => {
     'Milk',
     'Bread'
   ]
+  res.render('d1e3', {shopping})
 
   // TODO: Render 'd1e3.ejs', passing the value of `shopping
 })
